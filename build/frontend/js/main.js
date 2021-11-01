be861df3e6ecf8abbb699072b2a4b1c520d497fd
Irina403 <irinat403@gmail.com>
2021-11-01 18:38:11 +0300

    release1

--- build/frontend/js/main.js
+++ build/frontend/js/main.js
@@ -11,25 +11,5 @@ $(document).ready(function () {
 	  x.classList.toggle("change");
 	}
 
-	$('.myLinkModal').click(function(e) {
-		e.preventDefault()
-		target = '#' + $(this).attr('data-id')
-		// $(target).toggleClass('hidden')
-		$(".myOverlay").fadeIn(200, function () {
-			$(target).css("display", "block").animate({ opacity: 1 }, 198);
-		  });
-		
-		  $(".popup-close,.myOverlay").click(function () {
-			$(this).parents(target).fadeOut(30);
-			$(".myOverlay").fadeOut(30);
-			return false;
-		  });
-	  })
-	  
-	//   $('.myOverlay').click(function() {
-	// 	if (!$(this.target).is('.myModal')) {
-	// 	  $(".myModal").fadeOut(30);
-	// 	}
-	//   });
 
 });	
